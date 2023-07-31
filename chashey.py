8a74e1547cae230727c3cdb081092e0f3c62efc6
bhaithile <130885436+bhaithile@users.noreply.github.com>
2023-07-31 14:55:59 +0800

    Update chashey.py

--- chashey.py
+++ chashey.py
@@ -28,14 +28,22 @@ for key, sublist in input_dict.items():
     prev_first_num = first_num
     print(f"{key}: {sublist}")
 
-# result expected
+# function that does the task:
     10: [[6, 12, 25, 2, 31, 295], [13, 0, 20, 0, 0], [21, 47, 54, 3, 35, 412]],
     11: [[0, 6, 27, 0, 56, 660], [13, 4, 40, 0, 0], [22, 52, 12, 5, 10, 47]],
     12: [[1, 0, 28, 5, 15, 318], [13, 9, 0, 0, 0], [23, 56, 31, 0, 51, 389]],
     13: [[2, 54, 30, 3, 40, 683], [13, 13, 20, 0, 0], [24, 0, 49, 2, 26, 24]],
-    14: # Between the key numbers 13 and 14, the first number of the first list has been skipped and the second is smaller. so should repeat the key number.
-    14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]],
+    14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]], # Between the key numbers 13 and 14, the first number of the first list has been skipped and the second is smaller. so should repeat the key number.
     15: [[5, 44, 11, 5, 61, 504], [13, 22, 2, 4, 8], [26, 7, 50, 4, 13, 203]],
     16: [[5, 40, 35, 1, 20, 61], [13, 26, 26, 3, 27], [0, 9, 51, 2, 6, 646]], # this key_value pair should delete, because the first number of the first list is the same number second number of the list is smaller
     17: [[6, 40, 35, 1, 20, 61], [14, 26, 26, 3, 27], [2, 9, 51, 2, 6, 646]]
-    
+
+    # result expected with the return 
+    10: [[6, 12, 25, 2, 31, 295], [13, 0, 20, 0, 0], [21, 47, 54, 3, 35, 412]],
+    11: [[0, 6, 27, 0, 56, 660], [13, 4, 40, 0, 0], [22, 52, 12, 5, 10, 47]],
+    12:
+    12: [[1, 0, 28, 5, 15, 318], [13, 9, 0, 0, 0], [23, 56, 31, 0, 51, 389]],
+    13: [[2, 54, 30, 3, 40, 683], [13, 13, 20, 0, 0], [24, 0, 49, 2, 26, 24]],
+    14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]],
+    15: [[5, 44, 11, 5, 61, 504], [13, 22, 2, 4, 8], [26, 7, 50, 4, 13, 203]],
+    17: [[6, 40, 35, 1, 20, 61], [14, 26, 26, 3, 27], [2, 9, 51, 2, 6, 646]]
