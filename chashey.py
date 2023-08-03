f6f6d4557e03e63353f554aaa8ab1739e56c4f36
bhaithile <130885436+bhaithile@users.noreply.github.com>
2023-08-03 17:55:38 +0800

    repeating_skipping_key_number

--- chashey.py
+++ chashey.py
@@ -33,9 +33,11 @@ for key, sublist in input_dict.items():
     11: [[0, 6, 27, 0, 56, 660], [13, 4, 40, 0, 0], [22, 52, 12, 5, 10, 47]],
     12: [[1, 0, 28, 5, 15, 318], [13, 9, 0, 0, 0], [23, 56, 31, 0, 51, 389]],
     13: [[2, 54, 30, 3, 40, 683], [13, 13, 20, 0, 0], [24, 0, 49, 2, 26, 24]],
-    14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]], # Between the key numbers 13 and 14, the first number of the first list has been skipped and the second is smaller. so should repeat the key number.
+    # Between the key numbers 13 and 14, the first number of the first list has been skipped and the second is smaller. so should repeat the key number.
+    14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]], 
     15: [[5, 44, 11, 5, 61, 504], [13, 22, 2, 4, 8], [26, 7, 50, 4, 13, 203]],
-    16: [[5, 40, 35, 1, 20, 61], [13, 26, 26, 3, 27], [0, 9, 51, 2, 6, 646]], # this key_value pair should delete, because the first number of the first list is the same number second number of the list is smaller
+    16: [[5, 40, 35, 1, 20, 61], [13, 26, 26, 3, 27], [0, 9, 51, 2, 6, 646]],
+     # This key_value pair should delete, because the first number of the first list is the same number second number of the list is smaller (15 and 16)
     17: [[6, 40, 35, 1, 20, 61], [14, 26, 26, 3, 27], [2, 9, 51, 2, 6, 646]]
 
     # result expected with the return 
@@ -46,4 +48,5 @@ for key, sublist in input_dict.items():
     13: [[2, 54, 30, 3, 40, 683], [13, 13, 20, 0, 0], [24, 0, 49, 2, 26, 24]],
     14: [[4, 48, 49, 5, 47, 240], [13, 17, 40, 0, 0], [25, 4, 50, 0, 19, 467]],
     15: [[5, 44, 11, 5, 61, 504], [13, 22, 2, 4, 8], [26, 7, 50, 4, 13, 203]],
+
     17: [[6, 40, 35, 1, 20, 61], [14, 26, 26, 3, 27], [2, 9, 51, 2, 6, 646]]
